8b92b208f8a9e5c5bb559dcc5a09b6a89236ae0c
Denzel <denzeltlee@gmail.com>
2020-05-18 01:10:56 +0800

    Updates

---
+++ precache-manifest.b5ac2c3b8face603fdf1d33c158005eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2764d620225f63e9408a12ec23040771",
+    "url": "/random-color-picker/index.html"
+  },
+  {
+    "revision": "b50d322aad0caad26bd3",
+    "url": "/random-color-picker/static/css/main.acd176f4.chunk.css"
+  },
+  {
+    "revision": "d80cefdb3931ec2166d8",
+    "url": "/random-color-picker/static/js/2.2f935218.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-color-picker/static/js/2.2f935218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b50d322aad0caad26bd3",
+    "url": "/random-color-picker/static/js/main.8bb62b6a.chunk.js"
+  },
+  {
+    "revision": "b7d8553814e62a734454",
+    "url": "/random-color-picker/static/js/runtime-main.1a4d835b.js"
+  }
+]);
